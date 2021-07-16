84282083d804ee331ccaef7d15c2d7f42c8d8578
Angélica Venta <angelicaventap@gmail.com>
2021-07-16 12:20:13 -0500

    changes

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 // estas funciones son de ejemplo
 import data from './data/ghibli/ghibli.js'
-export const ghibliMovies = data.films.map(data => [`${data.title} ${data.poster} ${data.description}`]);
+export const ghibliPoster = data.films.map(data => [`${data.poster}`]);
 
   
 //};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
-import { ghibliMovies } from './data.js';
+import { ghibliPoster } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+document.querySelector(".homeWindow").addEventListener("click", () => {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("films").style.display = "none";     
+});
+
+document.querySelector(".filmographyWindow").addEventListener("click", () => {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("films").style.display = "flex";    
+});
+
+
+let films =document.getElementById("films");
+let posters = []
+
+data.films.forEach(movie => {
+    
+    posters += `
+        <div id = "films">
+            <img src="${movie.poster}" class= "posterContainer">
+            <h4 id = "title">${movie.title}</h4>
+        </div>
+    `
+    films.innerHTML = posters;
+    
+})
 
 
 console.log(data);
-console.log(ghibliMovies);
+console.log(ghibliPoster);
