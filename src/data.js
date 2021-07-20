40c36bf09f479bdac708a0a880dab656d25906b7
Angélica Venta <angelicaventap@gmail.com>
2021-07-20 12:02:59 -0500

    Cambios en JavaScript

--- src/data.js
+++ src/data.js
@@ -1,24 +1,51 @@
 // estas funciones son de ejemplo
-<<<<<<< HEAD
-import data from './data/ghibli/ghibli.js'
-export const ghibliPoster = data.films.map(data => [`${data.poster}`]);
-=======
->>>>>>> ed9cd2df77b80034bc0c135b7a9a0d6fdb8ee1a2
 
-export const maping = (data) => {
-  let posters2 = []
+// export const maping = (data) => {
+//   let posters2 = []
 
-  data.films.forEach(movie => {
-      posters2 += `
-        <div class="card_film">
-            <img src=${movie.poster}>
-            <h2>${movie.title}</h2>
-        </div>
-      `
-  })
-  return posters2
-};
+//   data.films.forEach(movie => {
+//       posters2 += `
+//         <div class="card_film">
+//             <img src=${movie.poster}>
+//             <h2>${movie.title}</h2>
+//         </div>
+//       `
+//   })
+//   return posters2
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const sortDataA = (data) => {
+//   const sortRelease = data.films.sort((a,b) => (a.title > b.title ? 1 : -1)); 
+//  return sortRelease;
+//  }
+// // export const alphabeticalSort = (data) => {
+// //   let result = data.films.sort((a, b) => {
+// //     const titleA = a.title
+// //     const titleB = b.title
+// //     if (titleA < titleB) {
+// //       return -1;
+// //     }
+// //     if (titleA > titleB) {
+// //       return 1
+// //     } else{
+// //       return 0;
+// //     }
+// //   })
+// //   return result;
+// // } 
+
+// export const sortData = (data) => {
+//    const sortRelease = data.films.sort((a,b) => (a.release_date > b.release_date ? 1 : -1)); 
+//   return sortRelease;
+  
+// } 
+
+const sortData = (data) =>  data.sort((a,b) => a.release_date > b.release_date ? 1 : -1); 
+  export const sortAscending = (data) => sortData(data);
+  export const sortDescending = (data) => sortAscending(data).reverse();
+
+  export const sortAlphabetic = (data) => data.sort((a,b) => a.title > b.title ? 1 : -1);
+
+const sortRate = (data) =>  data.sort((a,b) => a.rt_score - b.rt_score);
+  export const sortWorst = (data) =>  sortRate(data);
+  
--- src/main.js
+++ src/main.js
@@ -1,59 +1,74 @@
-<<<<<<< HEAD
-import { ghibliPoster } from './data.js';
-=======
-import { maping } from './data.js';
->>>>>>> ed9cd2df77b80034bc0c135b7a9a0d6fdb8ee1a2
+import { sortAscending, sortDescending, sortAlphabetic, sortWorst} from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-<<<<<<< HEAD
-document.querySelector(".homeWindow").addEventListener("click", () => {
-    document.getElementById("home").style.display = "block";
-    document.getElementById("films").style.display = "none";     
-});
-
-document.querySelector(".filmographyWindow").addEventListener("click", () => {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("films").style.display = "flex";    
-});
-
-
-let films =document.getElementById("films");
-let posters = []
-
-data.films.forEach(movie => {
-    
-    posters += `
-        <div id = "films">
-            <img src="${movie.poster}" class= "posterContainer">
-            <h4 id = "title">${movie.title}</h4>
-        </div>
-    `
-    films.innerHTML = posters;
-    
-})
-
-
-console.log(data);
-console.log(ghibliPoster);
-=======
 var films = document.getElementById('films')
 const movies_button = document.getElementById('movies_button');
 const home = document.getElementById('home')
-const home_button = document.getElementById('home_button')
-
+const home_button = document.getElementById('home_button');
+const selector = document.querySelector(".sortMovies");
 
 home_button.addEventListener('click', () => {
-    home.style.display = "flex";
+    home.style.display = "block";
     films.style.display = "none";
 })
 
-let moviesMaped = maping(data)
-films.innerHTML = moviesMaped
+function printFilms(data){
+    document.getElementById("films").innerHTML = data.map((movie) => `
+    <div class="card_film">
+        <img src=${movie.poster} class = "posterimg" alt="${movie.title}">
+        <h2>${movie.title}</h2>
+        <h3 class="releaseDate"> Release date: ${movie.release_date} </h3>
+        <span class = "rate"> Score: ${movie.rt_score}</span>  
+        <div class= "movie_info">
+        <p class="description"> Description: ${movie.description} </p>
+        <p class="director"> Director: ${movie.director} </p>
+        <p class="producer"> Producer: ${movie.producer} </p>
+        </div>
+    </div>
+    `)
+}
+
+printFilms(data.films);
+// let moviesMaped = maping(data);
+// films.innerHTML = moviesMaped; 
 
 movies_button.addEventListener('click', () => {
     home.style.display = "none";
     films.style.display = "flex";
 })
->>>>>>> ed9cd2df77b80034bc0c135b7a9a0d6fdb8ee1a2
+
+// const selectElement = document.querySelector(".sortMovies");
+// selectElement.addEventListener('change', (event) => {
+//     if(selectElement.value === "alphabetic"){
+//         event = sortDataA(data.films, selectElement.value);
+//         maping(event);  
+//     } 
+// });
+
+// const select = document.querySelector(".sortMovies");
+// select.addEventListener("change", sortMovies);
+
+// function sortMovies(){
+//     let sortValue = select.value;
+//     let sortTitles = [];
+//     if (sortValue === "alphabetic"){
+//         sortTitles = alphabeticalSort(data.films);        
+//     }
+//}
+selector.addEventListener("change", clasificar);
+function clasificar(){
+    let sortValue = selector.value;
+    let sortTitles = [];
+    if (sortValue === "releaseAscending"){
+        sortTitles = sortAscending(data.films);
+    } else if (sortValue === "releaseDescending"){
+        sortTitles = sortDescending(data.films);
+    } else if (sortValue === "alphabetic"){
+        sortTitles= sortAlphabetic(data.films);
+    } else if (sortValue === "score"){
+        sortTitles= sortWorst(data.films);
+    }
+    printFilms(sortTitles);
+}
